3da5f25d128522592a998b70bf1b92551e9c5a80
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2024-05-16 15:56:11 +0200

    fixed the token counting

--- Experiment/main.py
+++ Experiment/main.py
@@ -1,6 +1,6 @@
 from CreateQueryEngines import create_query_engines
 from DataLogging import DataLogging
-import json, tqdm, tiktoken, os
+import json, tiktoken, os
 from deepeval.integrations.llama_index import (
     DeepEvalAnswerRelevancyEvaluator,
     DeepEvalFaithfulnessEvaluator,
@@ -19,6 +19,13 @@ and save them to disk.
 
 llm = "gpt-3.5-turbo"
 
+# the token counter needs to be initialized first, before the query engines
+# otherwise it will log nothing
+token_counter = TokenCountingHandler(
+    tokenizer=tiktoken.encoding_for_model(llm).encode,
+    verbose=False,
+)
+Settings.callback_manager = CallbackManager([token_counter])
 
 # Create the query engines
 # for a more detailed description of the query engines and the possible variations that can be made,
@@ -47,13 +54,6 @@ contextualRelevancyEvaluator = DeepEvalContextualRelevancyEvaluator(threshold=th
 
 evaluators = [answerRelevancyEvaluator, faithfulnessEvaluator, contextualRelevancyEvaluator]
 
-# the token counter
-token_counter = TokenCountingHandler(
-    tokenizer=tiktoken.encoding_for_model(llm).encode,
-    verbose=False,
-)
-Settings.callback_manager = CallbackManager([token_counter])
-
 # the count is only reset manually, otherwise it would accumulate over multiple queries
 token_counter.reset_counts()
 
@@ -71,13 +71,24 @@ I will create a separate file for these as they will have a different structure
 and can't be easily compared to the other retrievers.
 """
 print("Starting Experiment")
-for qe_id, qe in tqdm.tqdm(fusion_only.items()):
+current_qe: int = 0
+total_amount = len(query_engines)
+total_amount_questions = len(questions)
+total_amount_evaluators = len(evaluators)
 
+for qe_id, qe in query_engines.items():
+    current_qe += 1
     # create the data logging object
     path = "logs/" + qe_id + ".csv"
     data_logger = DataLogging(file_path=path)
 
-    for question in tqdm.tqdm(questions):
+    # tqdm looked a bit ugly, so I'm creating my own outputs
+    print(f"Now starting Query Engine {current_qe} of {total_amount}: {qe_id.split("_2024")[0]}.")
+    current_question = 0
+
+    for question in questions:
+        current_question += 1
+        print(f"\tAsking question {current_question} of {total_amount_questions}")
 
         query = question["question"]
 
@@ -111,8 +122,9 @@ for qe_id, qe in tqdm.tqdm(fusion_only.items()):
         if add_data:
             data_logger.write_csv(add_data, add_path)
 
+        print("\t\tDone querying. Starting Evaluation.")
         # collect token counts
-        # TODO: it always returns 0, so something is off here
+        # TODO: check if the Fusion Retriever LLM call is also logged or not
         token_embeddings = token_counter.total_embedding_token_count
         token_prompt = token_counter.prompt_llm_token_count
         token_completion = token_counter.completion_llm_token_count
@@ -122,14 +134,13 @@ for qe_id, qe in tqdm.tqdm(fusion_only.items()):
                   "completion_tokens": token_completion,
                   "total_tokens": token_total}
         # for testing
-        print("Token Embeddings: " + str(token_embeddings))
         token_counter.reset_counts()    # do not forget to reset the counts!
 
         # evaluate the response
         evaluation = {}
+        current_evaluator = 0
         for evaluator in evaluators:
-            evaluation_result = evaluator.evaluate_response(query=query, response=response)
-
+            current_evaluator += 1
             # turn the results into a dictionary based on the name of the evaluator
             eval_name = str(evaluator)
             eval_name = eval_name.replace("Evaluator", "")
@@ -138,6 +149,9 @@ for qe_id, qe in tqdm.tqdm(fusion_only.items()):
             # so I need to trim it further
             eval_name = eval_name.split("evaluators.")[1]
             eval_name = eval_name.split(" object")[0]
+
+            print(f"\t\tStarting with Evaluator {current_evaluator} out of {total_amount_evaluators}: {eval_name}.")
+            evaluation_result = evaluator.evaluate_response(query=query, response=response)
 
             results = {eval_name + "_passing": evaluation_result.passing,
                        eval_name + "_feedback": evaluation_result.feedback,
---
+++ Experiment/token_counter.py
@@ -0,0 +1,162 @@
+"""
+for tests that need some experiment files imported
+"""
+import os
+
+import tiktoken
+from dotenv import load_dotenv
+from llama_index.core import Settings, StorageContext, VectorStoreIndex, get_response_synthesizer
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core.callbacks import TokenCountingHandler
+from llama_index.llms.openai import OpenAI
+from llama_index.vector_stores.elasticsearch import ElasticsearchStore
+
+from CreateQueryEngines import create_query_engines
+from CustomQueryEngine import ModifiedQueryEngine
+
+"""
+Update:
+It does work when the token counter is initialized in the same function as the query engine
+But not if they are made in separate functions. Maybe it is also the order of operation.
+
+so the token counter does not work with the CustomQueryEngine
+or at least not when created by the CreateQueryEngine method
+
+it does work when I just initialize and run the retriever and synthesizer in file
+when initializing an index in file the token counter increases even for the customQE
+although that particular index is not used by them...
+"""
+
+def main():
+    query_engines = create_query_engines()
+    llm = "gpt-3.5-turbo"
+
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    Settings.llm = OpenAI(model="gpt-3.5-turbo", api_key=api_key)
+
+    token_counter = TokenCountingHandler(
+        tokenizer=tiktoken.encoding_for_model(llm).encode,
+        verbose=False,
+    )
+    Settings.callback_manager = CallbackManager([token_counter])
+
+    es_vector_store = ElasticsearchStore(
+        index_name="city_service_store",
+        es_url="http://localhost:9200",
+    )
+
+    storage_context = StorageContext.from_defaults(vector_store=es_vector_store)
+
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=es_vector_store,
+        storage_context=storage_context,
+        show_progress=True)
+
+    retriever = index.as_retriever()
+    response_synth = get_response_synthesizer()
+
+    token_counter.reset_counts()
+
+    query = "Wie viel Perso?"
+    for name, qe in query_engines.items():
+        answer = qe.query(query)
+        token_embeddings = token_counter.total_embedding_token_count
+        token_completion = token_counter.completion_llm_token_count
+        print("Token Embeddings: " + str(token_embeddings))
+        print("Token Completion: " + str(token_completion))
+
+    print("in file:")
+    answer = response_synth.synthesize(query, retriever.retrieve(query))
+    token_embeddings = token_counter.total_embedding_token_count
+    token_completion = token_counter.completion_llm_token_count
+    print("Token Embeddings: " + str(token_embeddings))
+    print("Token Completion: " + str(token_completion))
+
+def CustomEngine():
+    llm = "gpt-3.5-turbo"
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    Settings.llm = OpenAI(model="gpt-3.5-turbo", api_key=api_key)
+
+    es_vector_store = ElasticsearchStore(
+        index_name="city_service_store",
+        es_url="http://localhost:9200",
+    )
+
+    storage_context = StorageContext.from_defaults(vector_store=es_vector_store)
+
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=es_vector_store,
+        storage_context=storage_context,
+        show_progress=True)
+
+    retriever = index.as_retriever()
+    response_synth = get_response_synthesizer()
+
+    query = "Wie viel Perso?"
+    qe = ModifiedQueryEngine(retriever=retriever, response_synthesizer=response_synth)
+
+    token_counter = TokenCountingHandler(
+        tokenizer=tiktoken.encoding_for_model(llm).encode,
+        verbose=False,
+    )
+    Settings.callback_manager = CallbackManager([token_counter])
+    qe.query(query)
+    token_embeddings = token_counter.total_embedding_token_count
+    token_completion = token_counter.completion_llm_token_count
+    print("Token Embeddings: " + str(token_embeddings))
+    print("Token Completion: " + str(token_completion))
+
+    qe.query(query)
+    token_embeddings = token_counter.total_embedding_token_count
+    token_completion = token_counter.completion_llm_token_count
+    print("Token Embeddings: " + str(token_embeddings))
+    print("Token Completion: " + str(token_completion))
+
+def getQE():
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    Settings.llm = OpenAI(model="gpt-3.5-turbo", api_key=api_key)
+
+    es_vector_store = ElasticsearchStore(
+        index_name="city_service_store",
+        es_url="http://localhost:9200",
+    )
+
+    storage_context = StorageContext.from_defaults(vector_store=es_vector_store)
+
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=es_vector_store,
+        storage_context=storage_context,
+        show_progress=True)
+
+    retriever = index.as_retriever()
+    response_synth = get_response_synthesizer()
+    qe = ModifiedQueryEngine(retriever=retriever, response_synthesizer=response_synth)
+    return qe
+
+def useQE():
+    query = "Was Personalausweis?"
+    llm = "gpt-3.5-turbo"
+    token_counter = TokenCountingHandler(
+        tokenizer=tiktoken.encoding_for_model(llm).encode,
+        verbose=False,
+    )
+    Settings.callback_manager = CallbackManager([token_counter])
+    qe = getQE()
+    r = qe.query(query)
+    qe.query(query)
+    token_embeddings = token_counter.total_embedding_token_count
+    token_completion = token_counter.completion_llm_token_count
+    print("Token Embeddings: " + str(token_embeddings))
+    print("Token Completion: " + str(token_completion))
+
+
+def printing():
+    print("normal")
+    print("\twith indent")
+    print("\twith indent")
+
+if __name__ == "__main__":
+    printing()
